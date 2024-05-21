2f930c8231b3102fa64396eba90b8810f1b4b8bb
mark <markpackham1@gmail.com>
2024-05-21 14:50:27 +0100

    Add Job class
    https://youtu.be/EE3xSdol38k?si=geFrY3TPkAfhFwE6&t=194

--- routes/web.php
+++ routes/web.php
@@ -4,26 +4,28 @@ use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 class Job {
-    
+    public static function all(): array{
+        return [['id' => 1, 'title' => 'Director', 'salary' => '$50,000'], ['id' => 2, 'title' => 'Janitor', 'salary' => '$10,000'], ['id' => 3, 'title' => 'Security', 'salary' => '$20,000']];
+
+    }
 }
 
-$jobs = [['id' => 1, 'title' => 'Director', 'salary' => '$50,000'], ['id' => 2, 'title' => 'Janitor', 'salary' => '$10,000'], ['id' => 3, 'title' => 'Security', 'salary' => '$20,000']];
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/jobs', function () use ($jobs) {
-    return view('jobs', ['jobs' => $jobs]);
+Route::get('/jobs', function ()  {
+    return view('jobs', ['jobs' => Job::all()]);
 });
 
-Route::get('/jobs/{id}', function ($id) use ($jobs){
+Route::get('/jobs/{id}', function ($id) {
     // die dump
     // dd($id);
 
 // Use short closure to access $id from above
 // closures use a similar style to JavaScript's arrow function but need "fn" at the start
-   $job = Arr::first($jobs, fn($job)=> $job['id'] == $id);
+   $job = Arr::first(Job::all(), fn($job)=> $job['id'] == $id);
     return view('job', ['job' => $job]);
 });
 
