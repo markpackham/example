926cf3a8f4c7da3228bca90f06f2602b20f1d3a8
mark <markpackham1@gmail.com>
2024-05-21 09:55:35 +0100

    Comment on closures like arrow functions

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/jobs/{id}', function ($id) {
 $jobs = [['id' => 1, 'title' => 'Director', 'salary' => '$50,000'], ['id' => 2, 'title' => 'Janitor', 'salary' => '$10,000'], ['id' => 3, 'title' => 'Security', 'salary' => '$20,000']];
 
 // Use short closure to access $id from above
+// closures use a similar style to JavaScript's arrow function but need "fn" at the start
    $job = Arr::first($jobs, fn($job)=> $job['id'] == $id);
     return view('job', ['job' => $job]);
 });
