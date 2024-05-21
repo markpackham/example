ad0e59319f5db15e36b9f8fbac2a8f865e136f05
mark <markpackham1@gmail.com>
2024-05-21 10:11:07 +0100

    Make jobs array global
    https://youtu.be/EE3xSdol38k?si=5hBq_vleBtM3IPoc&t=100

--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,23 @@
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
+class Job {
+    
+}
+
+$jobs = [['id' => 1, 'title' => 'Director', 'salary' => '$50,000'], ['id' => 2, 'title' => 'Janitor', 'salary' => '$10,000'], ['id' => 3, 'title' => 'Security', 'salary' => '$20,000']];
+
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/jobs', function () {
-    return view('jobs', ['jobs' => [['id' => 1, 'title' => 'Director', 'salary' => '$50,000'], ['id' => 2, 'title' => 'Janitor', 'salary' => '$10,000'], ['id' => 3, 'title' => 'Security', 'salary' => '$20,000']]]);
+Route::get('/jobs', function () use ($jobs) {
+    return view('jobs', ['jobs' => $jobs]);
 });
 
-Route::get('/jobs/{id}', function ($id) {
+Route::get('/jobs/{id}', function ($id) use ($jobs){
     // die dump
     // dd($id);
-$jobs = [['id' => 1, 'title' => 'Director', 'salary' => '$50,000'], ['id' => 2, 'title' => 'Janitor', 'salary' => '$10,000'], ['id' => 3, 'title' => 'Security', 'salary' => '$20,000']];
 
 // Use short closure to access $id from above
 // closures use a similar style to JavaScript's arrow function but need "fn" at the start
