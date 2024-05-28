8b3cab05d574ff7d055857d6f77b7058b6e10a3a
mark <markpackham1@gmail.com>
2024-05-28 18:22:52 +0100

    Demo use of Bootstrap 5 for pagination
    https://youtu.be/oLy1uXU1q7c?si=40jlmp2MnlVYf4ps&t=380

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
@@ -24,5 +25,8 @@ class AppServiceProvider extends ServiceProvider
         // force user to eager load instead
         // to reduce the number of SQL queries
         Model::preventLazyLoading();
+
+        // Change style used for Pagination - eg Bootstrap or the default Tailwind
+        // Paginator::useBootstrapFive();
     }
 }
