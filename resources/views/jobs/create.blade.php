4ec24f0fa9f40afaac5e3178a59c88bee94f8a3a
mark <markpackham1@gmail.com>
2024-06-04 18:38:22 +0100

    Add errors in template https://youtu.be/tROESL4trkQ?si=fVUyEyzhqPXpQIBg&t=614

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -37,6 +37,17 @@
                   class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                   placeholder="$20,000 Per Year">
               </div>
+
+              <div class="mt-10">
+                @if($errors->any())
+                <ul>
+                  @foreach ($errors->all() as $error)
+                    <li class="text-red-500 italic">{{$error}}</li>
+                  @endforeach
+                </ul>
+                @endif
+              </div>
+           
             </div>
           </div>
 
