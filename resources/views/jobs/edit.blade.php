96c08c523ae9a95a7ac093a7eb720b7457b28ecb
mark <markpackham1@gmail.com>
2024-06-05 13:51:30 +0100

    Use properties instead of array keys to show jobs
    https://youtu.be/syx1tWSZbL8?si=KzqrxFcam2EDWR6k&t=282

--- resources/views/jobs/edit.blade.php
+++ resources/views/jobs/edit.blade.php
@@ -19,9 +19,13 @@
               <div class="mt-2">
                 <div
                   class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                  <input type="text" name="title" id="title" autocomplete="title"
+                  <input type="text" name="title" 
+                  id="title" 
+                  autocomplete="title"
                     class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                    placeholder="Engineer" required>
+                    placeholder="Engineer" 
+                    value="{{$job->title}}"
+                    required>
                 </div>
   
                 {{-- Only concerns itself with title errors and no other ones --}}
@@ -38,8 +42,11 @@
               <div class="mt-2">
                 <div
                   class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                  <input type="text" name="salary" id="salary"
+                  <input type="text" 
+                  name="salary" 
+                  id="salary"
                     class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    value="{{$job->salary}}"
                     placeholder="$20,000 Per Year" required>
                 </div>
   
--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -3,11 +3,14 @@
         Job
     </x-slot:heading>
 
-    <h2 class="font-bold text-lg">{{$job['title']}}</h2>
+    <h2 class="font-bold text-lg">{{$job->title}}</h2>
 
-    <p>This job pays {{$job['salary']}} per year.</p>
+    {{-- When using the Eloquent ORM you can use both array keys eg $job['salary'] or properties $job->salary --}}
+    {{-- Just be consistant with which one you use --}}
+    {{-- Currently accessing via properties is the most trendy way to do things --}}
+    <p>This job pays {{$job->salary}} per year.</p>
 
     <p class="mt-6">
-        <x-button href="">Edit Job</x-button>
+        <x-button href="/jobs/{{$job->id}}/edit">Edit Job</x-button>
     </p>
 </x-layout>
