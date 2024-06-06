b49ca2215ff3f059f0b0c759fa73b4ea38cc4f20
mark <markpackham1@gmail.com>
2024-06-06 15:15:26 +0100

    Finish off routes work
    https://youtu.be/CtgVe-Q6KIY?si=SqIJ6EjY53fAn-Lc&t=96

--- routes/web.php
+++ routes/web.php
@@ -12,14 +12,28 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
 
-Route::controller(JobController::class)->group(function () {
-    Route::get('/jobs', 'index');
-    Route::get('/jobs/create', 'create');
-    Route::get('/jobs/{job}', 'show');
-    Route::post('/jobs', 'store');
-    Route::get('/jobs/{job}/edit', 'edit');
-    Route::patch('/jobs/{job}', 'update');
-    Route::delete('/jobs/{job}', 'destroy');
-});
+// Route::controller(JobController::class)->group(function () {
+//     Route::get('/jobs', 'index');
+//     Route::get('/jobs/create', 'create');
+//     Route::get('/jobs/{job}', 'show');
+//     Route::post('/jobs', 'store');
+//     Route::get('/jobs/{job}/edit', 'edit');
+//     Route::patch('/jobs/{job}', 'update');
+//     Route::delete('/jobs/{job}', 'destroy');
+// });
+
+// Route resource does the same for jobs as above
+// all action names follow defaults eg "show", "destroy", "index"
+Route::resource('jobs', JobController::class);
+
+
+// We can whitelist via 'only' and blacklist via 'except'
+// Route::resource('jobs', JobController::class, [
+//     'except' => ['edit']
+// ]);
+
+// Route::resource('jobs', JobController::class, [
+//     'only' => ['index', 'show']
+// ]);
 
 Route::view('/contact', 'contact');
