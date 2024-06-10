7c05062aba06287d106ae3c567792d8073510277
mark <markpackham1@gmail.com>
2024-06-10 11:45:21 +0100

    Add default content to template fields
    https://youtu.be/rsF-Z-XKqsc?si=d69uhFDymRKxrT0k&t=268

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -17,21 +17,10 @@
 
         <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
           <div class="sm:col-span-4">
-            <x-form-label />
-            <div class="mt-2">
-              <div
-                class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                <input type="text" name="title" id="title" autocomplete="title"
-                  class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                  placeholder="Engineer" required>
-              </div>
-
-              {{-- Only concerns itself with title errors and no other ones --}}
-              @error('title')
-              <p class="text-red-500 font-semibold mt-1">{{$message}}
-              </p>
-              @enderror
-            
+            <x-form-label for="title">Title</x-form-label>
+            <div name="title" id="title" class="mt-2">
+              <x-form-input name="title" id="title" placeholder="CEO" required></x-form-input>
+              <x-form-error name="title" />
             </div>
           </div>
 
@@ -55,19 +44,19 @@
                 <ul>
                   @foreach ($errors->all() as $error)
                     <li class="text-red-500 italic">{{$error}}</li>
-                  @endforeach
-                </ul>
-                @endif
-              </div> --}}
-           
-            </div>
-          </div>
+              @endforeach
+              </ul>
+              @endif
+            </div> --}}
 
-          <div class="mt-6 flex items-center justify-end gap-x-6">
-            <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
-            <button type="submit"
-              class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
           </div>
+        </div>
+
+        <div class="mt-6 flex items-center justify-end gap-x-6">
+          <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
+          <button type="submit"
+            class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+        </div>
   </form>
 
 </x-layout>
