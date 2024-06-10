07193ede000b9e279350f5d9a0f86aad0245022b
mark <markpackham1@gmail.com>
2024-06-10 14:59:31 +0100

    Add more fields for register form
    https://youtu.be/rsF-Z-XKqsc?si=iinndFFDWkD-Udp7&t=649

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,7 +8,7 @@
   
       <div class="space-y-12">
         <div class="border-b border-gray-900/10 pb-12"> 
-          <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+          <div class="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
   
             <x-form-field>
               <x-form-label for="first_name">First Name</x-form-label>
@@ -33,11 +33,27 @@
                   <x-form-error name="email" />
                 </div>
               </x-form-field>
+
+              <x-form-field>
+                <x-form-label for="password">Password</x-form-label>
+                <div class="mt-2">
+                  <x-form-input name="password" id="password" type="password" required></x-form-input>
+                  <x-form-error name="password" />
+                </div>
+              </x-form-field>
+
+              <x-form-field>
+                <x-form-label for="password_confirmation">Confirm Password</x-form-label>
+                <div class="mt-2">
+                  <x-form-input name="password_confirmation" id="password_confirmation" type="password" required></x-form-input>
+                  <x-form-error name="password_confirmation" />
+                </div>
+              </x-form-field>
   
           </div>
   
           <div class="mt-6 flex items-center justify-end gap-x-6">
-            <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
+            <a href="/" class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
             <x-form-button>Register</x-form-button>
           </div>
     </form>
