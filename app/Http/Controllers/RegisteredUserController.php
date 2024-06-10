a98624763782371d7d264d20553da9359df116d4
mark <markpackham1@gmail.com>
2024-06-10 15:52:21 +0100

    Add use register validation
    https://youtu.be/M8Vfm7hxqXA?si=Ld0ODw0G7rsws-43&t=302

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\Password;
 
 class RegisteredUserController extends Controller
 {
@@ -13,6 +14,14 @@ class RegisteredUserController extends Controller
 
     public function store()
     {
-        dd(request()->all());
+        request()->validate([
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => ['required', 'email', 'max:300'],
+            // When using "confirmed" laravel checks if it matches "password_confirmation"
+            // Can be applied to any attribute, if we used the "email" field then confirm would impact
+            // "email_confirmation"
+            'password' => ['required', Password::min(1), 'confirmed'],
+        ]);
     }
 }
