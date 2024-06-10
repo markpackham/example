df15b0c6f4a2d3db28975a132bd706779bec73ec
mark <markpackham1@gmail.com>
2024-06-10 15:11:51 +0100

    Add log in page template
    https://youtu.be/rsF-Z-XKqsc?si=fxNdBNEHe_YTm_zd&t=703

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,15 +1,15 @@
 <x-layout>
     <x-slot:heading>
-      Register
+      Log In
     </x-slot:heading>
   
-    <form method="POST" action="">
+    <form method="POST" action="/jobs">
       @csrf
   
       <div class="space-y-12">
         <div class="border-b border-gray-900/10 pb-12"> 
           <div class="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-
+  
             <x-form-field>
                 <x-form-label for="title">Email</x-form-label>
                 <div class="mt-2">
@@ -30,7 +30,7 @@
   
           <div class="mt-6 flex items-center justify-end gap-x-6">
             <a href="/" class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
-            <x-form-button>Register</x-form-button>
+            <x-form-button>Log In</x-form-button>
           </div>
     </form>
   
