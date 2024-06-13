cf8bfcbbdb24c5ef36ce85f1ac842099558f7f14
mark <markpackham1@gmail.com>
2024-06-13 15:27:51 +0100

    Improve email template
    https://youtu.be/OhQ_3yaUQRQ?si=ZGZh1VmEr7pXMbgU&t=63

--- resources/views/mail/job-posted.blade.php
+++ resources/views/mail/job-posted.blade.php
@@ -1,1 +1,10 @@
-Congrats! Your job is now live on our site!
+<h2>{{$job->title}}</h2>
+
+<p>
+    Congrats! Your job is now live on our site!
+</p>
+
+<p>
+    {{-- We have to use url so the full site url will appear in the user's email --}}
+    <a href="{{url('/job/' . $job->id)}}">View your job listing!</a>
+</p>
